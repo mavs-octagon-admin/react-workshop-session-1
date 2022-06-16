e832e092449a5085e5935ae30b4fafe227f84129
froggie4321 <heactora4321@gmail.com>
2022-06-16 07:25:30 -0500

    OT-67 Trying out react

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button,ImageBackground } from 'react-native';
+
+const image ={uri: "./assets/puppy-free-to-use.jpg"};
 
 export default function App() {
   return (
+  
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  
+	  
+      <Text>This just me making sure i can see changes on both IOS and Android</Text>
+	<Button
+	  onPress={()=>console.log("I am pressing button on app")}
+	  title = "Le ugly Button"
+	  color = "#AEE254"
+	  accessibilityLabel = "This is and ugly green button"
+	/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+container2: {
+    flex: 1,
+  },
+image: {
+    flex: 1,
+    justifyContent: "center"
+  },	
+	
 });
